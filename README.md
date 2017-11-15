3497e043800f10eac135af3acd010e5d81c6ae99
wuzhipeng <271737050@qq.com>
2017-11-15 11:28:27 +0800

    mysql samlpe test

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # epoll_ex
+编译mysqlexam.c 文件命令
+gcc -Wall mysqlexam.c -o mysqlexam -lmysqlclient
---
+++ mysqlexam.c
@@ -0,0 +1,40 @@
+#include <mysql/mysql.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char **argv)
+{
+	MYSQL *conn;
+	MYSQL_RES *res;
+	MYSQL_ROW row;
+	char server[] = "localhost";
+	char user[] = "root";
+	char password[] = "wzp#1234";
+	char database[] = "mysql";
+
+	conn = mysql_init(NULL);	
+	if(!mysql_real_connect(conn, server, user, password, database, 0, NULL, 0))
+	{
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	if(mysql_query(conn, "show tables"))
+	{
+		fprintf(stderr, "%s\n", mysql_error(conn));
+		exit(1);
+	}
+
+	res = mysql_use_result(conn);
+	printf("Mysql Tables in mysql database:\n");
+	
+	while((row  = mysql_fetch_row(res)) != NULL)
+	{
+		printf("%s \n", row[0]);
+	}
+
+	mysql_free_result(res);
+	mysql_close(conn);
+	printf("finish!\n");
+	return 0;
+}
